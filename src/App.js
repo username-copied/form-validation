93219420a6325c15112d3dc501cdb32353e063a6
roshgupta <gpta.duke.rohan.1219@gmail.com>
2024-01-23 15:01:27 +0530

    added fomik and basic layout for putting in form inputs

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ height: 100vh;
   width: 100%;
   height: 100%;
   .ring{
-    background-color: red;
+    background: rgb(255,105,163);
+    background: linear-gradient(90deg,rgba(255,33,138,1)  33%, rgba(255,105,163,1) 84%);
     border-radius:50%;
     position: absolute;
     bottom:10%;
@@ -39,6 +40,27 @@ height: 100vh;
     width: 200px;
     z-index: 2;
   }
+  .ring-1{
+    border-radius:33% 67% 47% 53% / 41% 66% 34% 59%;
+    left:2%;
+    bottom:40%;
+    width: 100px;
+    height: 100px;
+  }
+  .ring-2{
+    left:5%;
+    bottom:10%;
+    border-radius: 56% 44% 47% 53% / 50% 54% 46% 50% ;
+    width: 250px;
+    height: 250px;
+  }
+  .ring-3{
+    border-radius: 56% 44% 47% 53% / 50% 54% 46% 50% ;
+    width: 300px;
+    height: 300px;
+    bottom: 2%;
+    right: -2%;
+  }
 }
 
 `
@@ -46,6 +68,7 @@ height: 100vh;
 const StyledApp = styled.div`
   width: 100vw;
   height: 100vh;
+
   background-color: #171f38;
   color: whitesmoke;
   display: flex;
@@ -53,10 +76,11 @@ const StyledApp = styled.div`
   align-items: center;
   main{
     z-index: 9;
-    width: 80%;
-    height: 75%;
+    width: 90%;
+    height: 80%;
     background-color:#242e4cc9;
     border-radius: 8px ;
+    padding: 16px;
   }
 `
 
--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 const FormPage = () => {
-  return <div>FormPage</div>;
+  return (
+    <StyledFormContainer>
+      <header>UIDesignDaily</header>
+      <h1>Upload Files</h1>
+      <section></section>
+    </StyledFormContainer>
+  );
 };
+
+const StyledFormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  h1 {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default FormPage;
